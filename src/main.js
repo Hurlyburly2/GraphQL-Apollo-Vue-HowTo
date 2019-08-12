cc937cbb0f42c7d11b592de69c4eb11d7205b310
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-12 18:15:00 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import 'tachyons'
+import Vue from 'vue'
+
+import VueApollo from 'vue-apollo'
+
+import App from './App'
+import router from './router'
+
+require('dotenv').config()
+
+Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/' + process.env.SIMPLE_HTTP_LINK
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  provide: apolloProvider.provide(),
+  router,
+  render: h => h(App)
+})
